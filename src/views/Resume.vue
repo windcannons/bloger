611fc9bc9c6507668df675dd33cca2f709638e38
南极吃火锅 <2717741632@qq.com>
2025-03-03 11:46:17 +0800

    个人博客网站

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,7 +1,7 @@
 <template>
     <iframe
         class="w-100vw flex-1"
-        src="/public/web前端%20刘超%2017581724627.pdf"
+        :src="pdfUrl"
     ></iframe>
 </template>
 
@@ -9,6 +9,10 @@
 import { useWindowSize } from "@vueuse/core";
 const { height: windowHeight } = useWindowSize();
 
+// 使用绝对路径
+const pdfUrl = new URL("/public/web前端%20刘超%2017581724627.pdf", import.meta.url).href;
 </script>
+
 <style lang="less" scoped>
+/* 如果需要额外的样式，可以在这里添加 */
 </style>
